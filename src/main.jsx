fc7f5a0eaa0a1a056f0e2ecc1f832d70e783f518
JeanEmmanuelpaul <100246754+JeanEmmanuelpaul@users.noreply.github.com>
2026-05-06 13:07:11 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.scss'
+import App from './App.jsx'
+import { AdminAuthProvider } from './components/Admin/context/AdminAuth.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   
+       <App />
+ 
+   
+  </StrictMode>,
+)
